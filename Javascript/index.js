f0af9d0fbfd50044e134477ca9e8800b354a5e25
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2023-03-29 01:44:35 +0300

    Some Modifications

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,39 +1,69 @@
-
 // =================== Project Page ======================
+if(document.URL.includes("project.html")){
 
-// Removing An application Popup window
-let removeButtons = document.getElementsByClassName("removeBtn")
-document.getElementById("cancelRemoving").addEventListener("click", removePopup);
+    // Application form popup window (Apply Button)
+    document.querySelector("#cancelApplication").addEventListener("click", popup);
+    document.querySelector("#applicationBtn").addEventListener("click", popup);
+    function popup(){
+        document.getElementById("applicationWindow").classList.toggle("hidePopupWindow")
+    }
 
-for(i = 0; i<removeButtons.length;i++){
-    removeButtons[i].addEventListener("click", removePopup);
+    // Accepting An application Popup window (Accept Button)
+    let acceptButtons = document.getElementsByClassName("acceptBtn")
+    document.querySelector("#cancelAccepting").addEventListener("click", acceptPopup);
+
+    for(i = 0; i<acceptButtons.length;i++){
+        acceptButtons[i].addEventListener("click", acceptPopup);
+    }
+    function acceptPopup(){
+        document.querySelector("#acceptPopupWindow").classList.toggle("hidePopupWindow")
+    }
+
+
+    // Removing An application Popup window (Remove Button)
+    let removeButtons = document.getElementsByClassName("removeBtn") //Multible Elements
+    document.querySelector("#cancelRemoving").addEventListener("click", removePopup);
+
+    for(i = 0; i<removeButtons.length;i++){
+        removeButtons[i].addEventListener("click", removePopup);
+    }
+    function removePopup(){
+        document.querySelector("#removePopupWindow").classList.toggle("hidePopupWindow")
+    }
+
+    // Removing An application Popup window (Remove Button)
+    let portfolioButtons = document.getElementsByClassName("portfolioBtn") //Multible Elements
+
+    for(i = 0; i<removeButtons.length;i++){
+        portfolioButtons[i].addEventListener("click", () => {
+            window.location.href="portfolio.html";
+        });
+    }
+    
+
+
 }
-function removePopup(){
-    document.getElementById("removePopupWindow").classList.toggle("hidePopupWindow")
-}
-
-// Accepting An application Popup window
-let acceptButtons = document.getElementsByClassName("acceptBtn")
-document.getElementById("cancelAccepting").addEventListener("click", acceptPopup);
-
-for(i = 0; i<acceptButtons.length;i++){
-    acceptButtons[i].addEventListener("click", acceptPopup);
-}
-function acceptPopup(){
-    document.getElementById("acceptPopupWindow").classList.toggle("hidePopupWindow")
-}
-
-// Application form popup window
-document.getElementById("cancelApplication").addEventListener("click", popup);
-document.getElementById("applicationBtn").addEventListener("click", popup);
-function popup(){
-    document.getElementById("applicationWindow").classList.toggle("hidePopupWindow")
-}
-
 
 // =================== Setting Page ======================
-document.getElementById("cancelChangingPassword").addEventListener("click", popup);
-document.getElementById("changePassowrdBtn").addEventListener("click", popup);
-function popup(){
-    document.getElementById("changePasswordWindow").classList.toggle("hidePopupWindow")
+if(document.URL.includes("settings.html")){
+    document.querySelector("#cancelChangingPassword").addEventListener("click", changePasswordPopup);
+    document.querySelector("#changePassowrdBtn").addEventListener("click", changePasswordPopup);
+    function changePasswordPopup(){
+        document.querySelector("#changePasswordWindow").classList.toggle("hidePopupWindow")
+    }
 }
+
+
+
+// =================== My Project Page ======================
+
+if(document.URL.includes("my_projects.html")){
+    document.querySelector("#add-project-btn").addEventListener("click", () =>{
+        document.querySelector("#addProjectPopupWindow").classList.toggle("hidePopupWindow")
+    })
+
+    document.querySelector("#cancelAddingProject").addEventListener("click", () =>{
+        document.querySelector("#addProjectPopupWindow").classList.toggle("hidePopupWindow")
+    })
+}
+
