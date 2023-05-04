e9100fb14597eeb539858f7762794316f5cfee70
Turki <Shalahe30@gmail.com>
2023-05-05 00:32:13 +0300

    register DONE

--- db.js
+++ db.js
@@ -4,11 +4,14 @@ const connectionParms = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
-mongoose.connect('mongodb+srv://emad:123@teammatedb.oxmk3de.mongodb.net/Teammate?retryWrites=true&w=majority', connectionParms)
+const connect = ()=>{
+ mongoose.connect('mongodb+srv://emad:123@teammatedb.oxmk3de.mongodb.net/Teammate?retryWrites=true&w=majority', connectionParms)
 .then(()=>{
     console.log("Connected Successfully");
 })
 .catch((err) => {
     console.log(err);
 })
+}
 
+module.exports = connect
