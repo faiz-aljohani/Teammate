442fe4280f7204fbb9c1958788ae02350bbf3e91
nufox227 <nufox227@gmail.com>
2023-05-05 18:48:25 +0300

    portfolio now show all projects in the database dynamically

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const ProjectModel = require("../models/Projects").ProjectModel;
+const ProjectModel = require("../models/Projects.js");
+const database = require("../db");
+
+
 
 router.get("/", async (req,res) => {
-
+    console.log('GET req. in portfolio route')
     // getting user id from request/session
+    await database.connect();
     const Result = await ProjectModel.find({/* users: USER ID HERE */})//TODO: change this from finding all projects to only getting projects with the desired user 
-    console.log(Result);
-    console.log(typeof(Result));
+    // console.log(Result);
+    // console.log(typeof(Result));
     res.render("portfolio",{projects: Result})
 })
 
