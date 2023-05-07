9d6beb23f89ed5ba8355f228413a803253cdafe8
nufox227 <nufox227@gmail.com>
2023-05-07 21:42:11 +0300

    improvements on portfolio page

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,26 +1,76 @@
 const express = require('express')
 const router = express.Router()
 const ProjectModel = require("../models/Projects.js");
-const database = require("../db");
+// const database = require("../db");
+const app = express();
+const bodyParser = require("body-parser")
+const ejs = require("ejs")
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded());
 
+const path = require("path");
+const multer = require("multer")
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    },
+})
 
+const upload = multer({storage: storage});
+
+let userID;
 
 router.get("/", async (req,res) => {
     if(!isSessionActive(req))
         res.redirect("/login");
     else{
         console.log('GET req. in portfolio route')
-        // getting user id from request/session
-        await database.connect();
-        const Result = await ProjectModel.find({/* users: USER ID HERE */})//TODO: change this from finding all projects to only getting projects with the desired user
+        userID = req.session.userID;
+        const Result = await ProjectModel.find({userID: userID, completed: true})//TODO: change this from finding all projects to only getting projects with the desired user
         
-        // console.log(Result);
-        // console.log(typeof(Result));
         res.render("portfolio",{projects: Result}) 
     }
 
 })
-router.post("/", async (req,res) =>{
+router.post("/",upload.array("images"), async (req,res) =>{
     const addPrevProjectForm = req.body;
+    // console.log(addPrevProjectForm)
+    // Just to work with multiple files
+    let path = "";
+    console.log(req.files)
+    if(req.files){
+        req.files.forEach(file => {
+            path = path + file.path + ",";
+        })
+        path = path.substring(0, path.lastIndexOf(","))
+        console.log(path);
+    }
+
+    addPrevProject(addPrevProjectForm, path)
+    res.redirect("/portfolio")
 })
+
+function addPrevProject(addPrevProjectForm, imgs){
+    const title = addPrevProjectForm.title;
+    const description = addPrevProjectForm.description;
+    const skills = (addPrevProjectForm.skills).split(",");
+
+    const newProject = new ProjectModel({
+        userID: userID,
+        title: title,
+        description: description,
+        img: imgs,
+        skills: skills,
+        teammates: [],
+        established: true,
+        completed: true,
+        applications: []
+      })
+    console.log(newProject)
+    newProject.save()
+}
+
 module.exports = router;
