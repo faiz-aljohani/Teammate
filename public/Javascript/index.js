3eb2edeef76419f803c2138214460fa2255403a5
Emad Albalawi <sksko2013@hotmail.com>
2023-05-12 19:13:20 +0300

    fix Home page images, modifications in chat style

--- public/Javascript/index.js
+++ public/Javascript/index.js
@@ -1,38 +1,9 @@
 // =================== Project Page ======================
 if(document.URL.includes("/projects/")){
 
-    // Application form popup window (Apply Button)
-    document.querySelector("#cancelApplication").addEventListener("click", popup);
-    document.querySelector("#applicationBtn").addEventListener("click", popup);
-    function popup(){
-        document.getElementById("applicationWindow").classList.toggle("hidePopupWindow")
-    }
-
-    // Accepting An application Popup window (Accept Button)
-    let acceptButtons = document.getElementsByClassName("acceptBtn")
-    document.querySelector("#cancelAccepting").addEventListener("click", acceptPopup);
-
-    for(i = 0; i<acceptButtons.length;i++){
-        acceptButtons[i].addEventListener("click", acceptPopup);
-    }
-    function acceptPopup(){
-        document.querySelector("#acceptPopupWindow").classList.toggle("hidePopupWindow")
-    }
 
 
-      // // Removing  project Popup window (Remove Button)
-      // let removeProject = document.querySelector("#removeProject").addEventListener("click", removePopup)
 
-    // Removing An application Popup window (Remove Button)
-    let removeButtons = document.getElementsByClassName("removeBtn") //Multible Elements
-    document.querySelector("#cancelRemoving").addEventListener("click", removePopup);
-
-    for(i = 0; i<removeButtons.length;i++){
-        removeButtons[i].addEventListener("click", removePopup);
-    }
-    function removePopup(){
-        document.querySelector("#removePopupWindow").classList.toggle("hidePopupWindow")
-    }
 
     // Removing An application Popup window (Remove Button)
     let portfolioButtons = document.getElementsByClassName("portfolioBtn") //Multible Elements
@@ -45,6 +16,14 @@ if(document.URL.includes("/projects/")){
     
 
 
+}
+
+function applyPopup(){
+  document.getElementById("applicationWindow").classList.toggle("hidePopupWindow")
+}
+
+function completeProject(){
+  document.querySelector("#completePopupWindow").classList.toggle("hidePopupWindow") 
 }
 
 function removeProject(){
