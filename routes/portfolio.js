086a97aba5b9407c95f283b208587b06631d0d64
Emad Albalawi <sksko2013@hotmail.com>
2023-05-13 16:39:10 +0300

    portfolio modifications

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -37,6 +37,11 @@ router.get("/", async (req,res) => {
     else{
         console.log('GET req. in portfolio route')
         userID = req.session.userID;
+        let userName
+        await UserModel.findOne({_id: userID}).then(user => {
+            userName = user.firstName + " " + user.lastName;
+        })
+
         const Result = await ProjectModel.find({
                 completed: true,
                 $or: [ 
@@ -45,7 +50,7 @@ router.get("/", async (req,res) => {
                 ]
         })
         try{
-            res.render("portfolio",{projects: Result, viewerID: userID, ownerID: userID, ownerFirstName: ""})
+            res.render("portfolio",{projects: Result, viewerID: userID, ownerID: userID, userName: userName})
         }catch(error){
             console.log("you did somethign wrong!")
             // console.log(error)
@@ -76,12 +81,9 @@ router.get("/:id",async (req, res)=>{
             return;
         });
         if (user != null){
-            let ownerFirstName = user.firstName;
-            //Capitalize the first letter
-            ownerFirstName = 
-            ownerFirstName.charAt(0).toUpperCase()
-                + ownerFirstName.slice(1);
-            res.render("portfolio",{projects: Result, viewerID: userID, ownerID: ownerID, ownerFirstName: ownerFirstName}) ;
+            let userName = user.firstName + " " + user.lastName;
+
+            res.render("portfolio",{projects: Result, viewerID: userID, ownerID: ownerID, userName: userName}) ;
         }
         
     }
